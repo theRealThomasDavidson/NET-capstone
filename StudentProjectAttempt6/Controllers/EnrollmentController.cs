e9116b9389a2a0f0ba78115d8fc8229094acfbc5
theRealThomasDavidson <davidson.thomasj@gmail.com>
2022-12-08 15:55:53 -0500

    enrollment views ready

--- StudentProjectAttempt6/Controllers/EnrollmentController.cs
+++ StudentProjectAttempt6/Controllers/EnrollmentController.cs
@@ -1,8 +1,9 @@
 ﻿using StudentProjectAttempt6.Data;
-using Microsoft.AspNetCore.Mvc;
 using StudentProjectAttempt6.Models;
+using StudentProjectAttempt6.Models.ViewModels;
 using StudentProjectAttempt6.Repository.IRepository;
-
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace EnrollmentProjectAttempt6.Controllers
 {
@@ -17,36 +18,50 @@ namespace EnrollmentProjectAttempt6.Controllers
         public IActionResult Index()
         {
             IEnumerable<Enrollment> objEnrollmentList = _unitOfWork.Enrollment.GetAll();
+            foreach(Enrollment objEnrollment in objEnrollmentList)
+            {
+                objEnrollment.Student = _unitOfWork.Student.GetFirstOrDefault(i => i.Id == objEnrollment.StudentId);
+            }
             return View(objEnrollmentList);
         }
         public IActionResult Create()
         {
-            ViewData["Students"] = _unitOfWork.Student.GetAll();
-            return View();
+            EnrollmentViewModel modelVM = new()
+            {
+                Enrollment = new(),
+
+                StudentsList = _unitOfWork.Student.GetAll().Select(i =>
+                    new SelectListItem
+                    {
+                        Text = i.Name,
+                        Value = i.Id.ToString()
+                    }
+                ),
+
+            };
+            //ViewBag.StudentsList = StudentsList;
+            
+            return View(modelVM);
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Create(Enrollment obj)
+        public IActionResult Create(EnrollmentViewModel obj)
         {
-            if (obj.Grade > 4 || obj.Grade < 0) // Custom Validation
+            Enrollment enroll = obj.Enrollment;
+            if (enroll.Grade > 4 || enroll.Grade < 0) // Custom Validation
             {
                 ModelState.AddModelError("CustomError", "Enrollments GPA must be within 0 and 4.");
                 // ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
             }
-
-            if (ModelState.IsValid)
-            {
-                //_db.Enrollments.Add(obj);
-                //_db.SaveChanges();
-                //_db.Add(obj);
-                //_db.Save();
-                _unitOfWork.Enrollment.Add(obj);
-                _unitOfWork.Save();
-                TempData["success"] = "Enrollment added successfully";
-                return RedirectToAction("Index");
-            }
-
-            return View(obj);
+            //_db.Enrollments.Add(obj);
+            //_db.SaveChanges();
+            //_db.Add(obj);
+            //_db.Save();
+            _unitOfWork.Enrollment.Add(enroll);
+            _unitOfWork.Save();
+            TempData["success"] = "Enrollment added successfully";
+            return RedirectToAction("Index");
+            
         }
 
         public IActionResult Edit(int? id)
@@ -55,38 +70,53 @@ namespace EnrollmentProjectAttempt6.Controllers
             {
                 return NotFound();
             }
-
+       
             //Enrollment EnrollmentFromDb = _db.GetFirstOrDefault(x => x.Id == id);
             Enrollment enrollmentFromDb = _unitOfWork.Enrollment.GetFirstOrDefault(x => x.Id == id);
-
             if (enrollmentFromDb == null)
             {
                 return NotFound();
             }
-            ViewData["Students"] = _unitOfWork.Student.GetAll();
-            return View(enrollmentFromDb);
+            enrollmentFromDb.Id = (int)id;
+            EnrollmentViewModel modelVM = new()
+            {
+                Enrollment = enrollmentFromDb,
+
+                StudentsList = _unitOfWork.Student.GetAll().Select(i =>
+                    new SelectListItem
+                    {
+                        Text = i.Name,
+                        Value = i.Id.ToString()
+                    }
+                ),
+
+            };
+            return View(modelVM);
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Edit(Enrollment enrollment)
+        public IActionResult Edit(int? id, EnrollmentViewModel enrollmentVM)
         {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Enrollment enrollment = enrollmentVM.Enrollment;
+            enrollment.Id = (int)id;
             if (enrollment.Grade > 4 || enrollment.Grade < 0)
             {
                 ModelState.AddModelError("CustomError", "Enrollments GPA must be within 0 and 4.");
             }
-            if (ModelState.IsValid)
-            {
-                //_db.Enrollments.Update(Enrollment); 
-                //_db.SaveChanges();
-                //_db.Update(Enrollment); //for repo
-                //_db.Save();
-                _unitOfWork.Enrollment.Update(enrollment);
-                _unitOfWork.Save();
+            //_db.Enrollments.Update(Enrollment); 
+            //_db.SaveChanges();
+            //_db.Update(Enrollment); //for repo
+            //_db.Save();
+            _unitOfWork.Enrollment.Update(enrollment);
+            _unitOfWork.Save();
 
-                TempData["success"] = "Enrollment Modified successfully";
-                return RedirectToAction("Index");
-            }
-            return View(enrollment);
+            TempData["success"] = "Enrollment Modified successfully";
+            return RedirectToAction("Index");
         }
         public IActionResult Delete(int? id)
         {
@@ -100,6 +130,7 @@ namespace EnrollmentProjectAttempt6.Controllers
             {
                 return NotFound();
             }
+            enrollmentFromDb.Student = _unitOfWork.Student.GetFirstOrDefault(s=> s.Id == enrollmentFromDb.StudentId);
             return View(enrollmentFromDb);
         }
         [HttpPost, ActionName("Delete")]
---
+++ StudentProjectAttempt6/Models/ViewModels/EnrollmentViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+
+namespace StudentProjectAttempt6.Models.ViewModels
+{
+    public class EnrollmentViewModel
+    {
+        public Enrollment Enrollment { get; set; }
+
+        public IEnumerable<SelectListItem> StudentsList;
+    }
+}
